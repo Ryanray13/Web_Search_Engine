9964e0f7e3391328e871bc6bd90dc7519f38b378
Wuping Lei <wuping.lei@nyu.edu>
2014-09-29 00:28:13 -0400

    finish phrase ranker

--- src/edu/nyu/cs/cs2580/QueryHandler.java
+++ src/edu/nyu/cs/cs2580/QueryHandler.java
@@ -12,90 +12,95 @@ import java.util.Iterator;
 import java.util.Vector;
 
 class QueryHandler implements HttpHandler {
-  private static String plainResponse =
-      "Request received, but I am not smart enough to echo yet!\n";
+  private static String plainResponse = "Request received, but I am not smart enough to echo yet!\n";
 
   private Ranker _ranker;
 
-  public QueryHandler(Ranker ranker){
+  public QueryHandler(Ranker ranker) {
     _ranker = ranker;
   }
 
-  public static Map<String, String> getQueryMap(String query){  
-    String[] params = query.split("&");  
-    Map<String, String> map = new HashMap<String, String>();  
-    for (String param : params){  
-      String name = param.split("=")[0];  
-      String value = param.split("=")[1];  
-      map.put(name, value);  
+  public static Map<String, String> getQueryMap(String query) {
+    String[] params = query.split("&");
+    Map<String, String> map = new HashMap<String, String>();
+    for (String param : params) {
+      String name = param.split("=")[0];
+      String value = param.split("=")[1];
+      map.put(name, value);
     }
-    return map;  
-  } 
-  
+    return map;
+  }
+
   public void handle(HttpExchange exchange) throws IOException {
     String requestMethod = exchange.getRequestMethod();
-    if (!requestMethod.equalsIgnoreCase("GET")){  // GET requests only.
+    if (!requestMethod.equalsIgnoreCase("GET")) { // GET requests only.
       return;
     }
 
     // Print the user request header.
     Headers requestHeaders = exchange.getRequestHeaders();
     System.out.print("Incoming request: ");
-    for (String key : requestHeaders.keySet()){
+    for (String key : requestHeaders.keySet()) {
       System.out.print(key + ":" + requestHeaders.get(key) + "; ");
     }
     System.out.println();
-    String queryResponse = "";  
+    String queryResponse = "";
     String uriQuery = exchange.getRequestURI().getQuery();
     String uriPath = exchange.getRequestURI().getPath();
 
-    if ((uriPath != null) && (uriQuery != null)){
-      if (uriPath.equals("/search")){
-        Map<String,String> query_map = getQueryMap(uriQuery);
+    if ((uriPath != null) && (uriQuery != null)) {
+      if (uriPath.equals("/search")) {
+        Map<String, String> query_map = getQueryMap(uriQuery);
         Set<String> keys = query_map.keySet();
-        Vector < ScoredDocument > sds = new Vector < ScoredDocument >();
-        if (keys.contains("query")){
-          if (keys.contains("ranker")){
+        Vector<ScoredDocument> sds = new Vector<ScoredDocument>();
+        if (keys.contains("query")) {
+          if (keys.contains("ranker")) {
             String ranker_type = query_map.get("ranker");
             // @CS2580: Invoke different ranking functions inside your
             // implementation of the Ranker class.
-            if (ranker_type.equals("cosine")){
-              sds = _ranker.runquery(query_map.get("query"), Ranker.RankerType.COSINE);
-            } else if (ranker_type.equals("QL")){
-              sds = _ranker.runquery(query_map.get("query"), Ranker.RankerType.QL);
-            } else if (ranker_type.equals("phrase")){
-              queryResponse = (ranker_type + " not implemented.");
-            } else if (ranker_type.equals("linear")){
+            if (ranker_type.equals("cosine")) {
+              sds = _ranker.runquery(query_map.get("query"),
+                  Ranker.RankerType.COSINE);
+            } else if (ranker_type.equals("QL")) {
+              sds = _ranker.runquery(query_map.get("query"),
+                  Ranker.RankerType.QL);
+            } else if (ranker_type.equals("phrase")) {
+              sds = _ranker.runquery(query_map.get("query"),
+                  Ranker.RankerType.PHRASE);
+            } else if (ranker_type.equals("linear")) {
               queryResponse = (ranker_type + " not implemented.");
             } else {
-              queryResponse = (ranker_type+" not implemented.");
+              queryResponse = (ranker_type + " not implemented.");
             }
           } else {
-            // @CS2580: The following is instructor's simple ranker that does not
+            // @CS2580: The following is instructor's simple ranker that does
+            // not
             // use the Ranker class.
-            sds = _ranker.runquery(query_map.get("query"),Ranker.RankerType.COSINE);
+            sds = _ranker.runquery(query_map.get("query"),
+                Ranker.RankerType.COSINE);
           }
         }
-        Iterator < ScoredDocument > itr = sds.iterator();
-        while (itr.hasNext()){
+        Iterator<ScoredDocument> itr = sds.iterator();
+        while (itr.hasNext()) {
           ScoredDocument sd = itr.next();
-          if (queryResponse.length() > 0){
+          if (queryResponse.length() > 0) {
             queryResponse = queryResponse + "\n";
           }
-          queryResponse = queryResponse + query_map.get("query") + "\t" + sd.asString();
+          queryResponse = queryResponse + query_map.get("query") + "\t"
+              + sd.asString();
         }
-        if (queryResponse.length() > 0){
+        if (queryResponse.length() > 0) {
           queryResponse = queryResponse + "\n";
         }
       }
     }
-    
-      // Construct a simple response.
-      Headers responseHeaders = exchange.getResponseHeaders();
-      responseHeaders.set("Content-Type", "text/plain");
-      exchange.sendResponseHeaders(200, 0);  // arbitrary number of bytes
-      OutputStream responseBody = exchange.getResponseBody();
-      responseBody.write(queryResponse.getBytes());
-      responseBody.close();
+
+    // Construct a simple response.
+    Headers responseHeaders = exchange.getResponseHeaders();
+    responseHeaders.set("Content-Type", "text/plain");
+    exchange.sendResponseHeaders(200, 0); // arbitrary number of bytes
+    OutputStream responseBody = exchange.getResponseBody();
+    responseBody.write(queryResponse.getBytes());
+    responseBody.close();
   }
 }
